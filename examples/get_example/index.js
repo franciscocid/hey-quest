49252bbbb15add67d53ed9c2235df1bceecdb9ac
Francisco Cid <juniorfchj2@gmail.com>
2019-08-02 11:39:11 -0300

    Fixed issues on hey.js

---
+++ examples/get_example/index.js
@@ -0,0 +1,13 @@
+const hey = require('hey')
+
+async function get_example(){
+	try{
+		const res = await hey('https://jsonplaceholder.typicode.com/todos/1').do()
+		const json = await res.json()
+		console.log(json)
+	}catch(e){
+		console.error(e)
+	}
+}
+example()
+
