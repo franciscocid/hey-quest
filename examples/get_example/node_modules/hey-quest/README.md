3ac952d6b346d2fed435ca74699b12cc0ea1df40
Francisco Cid <juniorfchj2@gmail.com>
2019-08-02 11:43:55 -0300

    Example folder and simple GET example.

--- examples/get_example/node_modules/hey-quest/README.md
+++ examples/get_example/node_modules/hey-quest/README.md
@@ -11,3 +11,10 @@ A light-weight module that aims to make HTTP requests clean, intuitive, reusable
 - [ ] Handle common headers
 - [ ] Use babel and maybe a Webpack
 - [ ] Be used for the first time! :D
+
+## Documentation:
+
+- [ ] NPM Keywords
+- [ ] NPM Examples
+- [ ] Examples folder
+- [ ] Better texts and stuff
--- examples/get_example/node_modules/hey-quest/src/index.js
+++ examples/get_example/node_modules/hey-quest/src/index.js
@@ -1,14 +1,1 @@
-const hey = require('./hey')
-
-async function example(){
-	try{
-		const res = await hey('https://jsonplaceholder.typicode.com/todos/1').do()
-		const json = await res.json()
-		console.log(json)
-	}catch(e){
-		console.error(e)
-	}
-}
-example()
-
-module.exports = hey
+module.exports = require('./hey')
